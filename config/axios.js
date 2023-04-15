85018c14c0adf08e34a20039c72c0b83e8461b89
Borisbam <borisbam95@gmail.com>
2023-04-15 09:39:36 +0000

    next config

--- config/axios.js
+++ config/axios.js
@@ -4,4 +4,6 @@ const clientAxios = axios.create({
     baseURL: process.env.backendURL
 });
 
+console.log(backendURL)
+
 export default clientAxios;
--- next.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    env: {
-        backendURL: 'https://borisbam-nodesend-server.herokuapp.com/',
-        frontendURL: 'https://nodesend-client-jegkegfj7-borisbam.vercel.app/'
-    }
-}
